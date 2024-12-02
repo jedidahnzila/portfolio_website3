aabde95e728c210080f36b2d1a667687635685cd
jedidah <nzilajedidah@gmail.com>
2024-12-02 16:02:42 +0300

    added home page details

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to My Portfolio Website</h1>
+      <p>This is the home page of my React portfolio.</p>
+      <p>Stay tuned for more updates!</p>
+    </div>
+  );
+};
+
+export default Home;
